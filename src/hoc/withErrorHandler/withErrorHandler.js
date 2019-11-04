0bb1956b2a12df900af164aa18c2fba243f31069
alexeybokov <lexabokov18@gmail.com>
2019-11-04 22:20:56 +0200

    interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -11,13 +11,19 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
         }
 
         componentWillMount() {
-            axios.interceptors.request.use(req => {
+            this.reqInteceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.resIteceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             });
+        }
+
+        componentWillUnmount() {
+            console.log('Will Unmount', this.reqInterceptor, this.resInterceptor);
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
