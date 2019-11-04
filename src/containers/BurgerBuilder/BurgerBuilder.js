809442b73eed73a50038646f6ab18e324202c6ba
alexeybokov <lexabokov18@gmail.com>
2019-11-04 11:47:50 +0200

    send POST request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -77,12 +78,29 @@ class BurgerBuilder extends Component {
       this.setState({purchasing: true});
   }
 
-  purchaseCancelHandler =() => {
+  purchaseCancelHandler = () => {
       this.setState({purchasing: false});
   }
 
-  purchaseContinueHandler =() => {
-      alert('You continue!');
+  purchaseContinueHandler = () => {
+      // alert('You continue!');
+      const order = {
+          ingredients: this.state.ingredients,
+          price: this.state.totalPrice,
+          customer: {
+              name: 'Alex',
+              address: {
+                  street: 'Test 1',
+                  zipCode: '54321',
+                  country: 'Ukraine'
+              },
+              email: 'test@gmail.com'
+          },
+          deliveryMethod: 'Normal'
+      }
+      axios.post('/orders.json', order)
+          .then(response => console.log(response))
+          .catch(error => console.log(error));
   }
 
   render () {
