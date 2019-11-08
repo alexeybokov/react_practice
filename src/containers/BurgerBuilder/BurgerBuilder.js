fdba2e7bc7ddb3de718f83cb96275d0146001fb2
alexeybokov <lexabokov18@gmail.com>
2019-11-08 13:12:35 +0200

    passing data via query params

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -116,8 +116,14 @@ class BurgerBuilder extends Component {
       //     .catch( error => {
       //         this.setState({ loading: false });
       //     });
+      const queryParams = [];
+      for (let i in this.state.ingredients) {
+          queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+      }
+      const queryString = queryParams.join('&');
       this.props.history.push({
-          pathname: '/checkout'
+          pathname: '/checkout',
+          search: '?' + queryString
       });
   }
 
