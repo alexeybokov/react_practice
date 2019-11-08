5302ff4530e5d7e3dda0d45f093e9497f4c6d3a3
alexeybokov <lexabokov18@gmail.com>
2019-11-08 14:48:10 +0200

    passing data between pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,32 +94,12 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
       // alert('You continue!');
-      // this.setState({ loading: true });
-      // const order = {
-      //     ingredients: this.state.ingredients,
-      //     price: this.state.totalPrice,
-      //     customer: {
-      //         name: 'Alex',
-      //         address: {
-      //             street: 'Test 1',
-      //             zipCode: '54321',
-      //             country: 'Ukraine'
-      //         },
-      //         email: 'test@gmail.com'
-      //     },
-      //     deliveryMethod: 'Normal'
-      // }
-      // axios.post('/orders.json', order)
-      //     .then(response => {
-      //         this.setState({ loading: false, purchasing: false });
-      //     })
-      //     .catch( error => {
-      //         this.setState({ loading: false });
-      //     });
+
       const queryParams = [];
       for (let i in this.state.ingredients) {
           queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
       }
+      queryParams.push('price=' + this.state.totalPrice);
       const queryString = queryParams.join('&');
       this.props.history.push({
           pathname: '/checkout',
