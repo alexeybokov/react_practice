2a9856b22d57d223235dd159dd5233eac6197d48
alexeybokov <lexabokov18@gmail.com>
2019-11-13 18:09:09 +0200

    handling user input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,13 +10,17 @@ const input = ( props ) => {
             inputElement = <input
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed}
+
+            />;
             break;
         case ( 'textarea' ):
             inputElement = <textarea
                 className={classes.InputElement}
                 {...props.elementConfig}
                 value={props.value}
+                onChange={props.changed}
             />;
             break;
 
