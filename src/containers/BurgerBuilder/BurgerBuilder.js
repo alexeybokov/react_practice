26a2ea3e3b2c28b0d2974c619bc1e3181ec69107
alexeybokov <lexabokov18@gmail.com>
2019-11-27 12:51:32 +0200

    forwading unauthenticated users

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,11 +38,15 @@ class BurgerBuilder extends Component {
   }
 
   purchaseHandler = () => {
-      this.setState({purchasing: true});
+      if (this.props.isAuthenticated) {
+          this.setState({ purchasing: true } );
+      } else {
+          this.props.history.push('/auth');
+      }
   }
 
   purchaseCancelHandler = () => {
-      this.setState({purchasing: false});
+      this.setState({ purchasing: false } );
   }
 
   purchaseContinueHandler = () => {
@@ -51,7 +55,7 @@ class BurgerBuilder extends Component {
   }
 
   render () {
-    const disabledInfo ={
+    const disabledInfo = {
         ...this.props.ings
     };
     for (let key in disabledInfo) {
@@ -70,6 +74,7 @@ class BurgerBuilder extends Component {
                     disabled={disabledInfo}
                     purchasable={this.updatePurchaseState(this.props.ings)}
                     ordered={this.purchaseHandler}
+                    isAuth={this.props.isAuthenticated}
                     price={this.props.price} />
             </Aux>
         );
@@ -95,7 +100,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     };
 }
 
