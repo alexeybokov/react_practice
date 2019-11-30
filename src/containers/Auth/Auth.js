05fcf9b74afb32e2b318a68d0d240eb8dfb50f79
alexeybokov <lexabokov18@gmail.com>
2019-11-30 22:30:01 +0200

    sharing validation method

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,7 +7,7 @@ import Button from '../../components/UI/Button/Button';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.module.css';
 import * as actions from '../../store/actions/index';
-import { updateObject } from '../../shared/utility';
+import { updateObject, checkValidity } from '../../shared/utility';
 
 class Auth extends Component {
     state = {
@@ -50,48 +50,11 @@ class Auth extends Component {
         }
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-        if(!rules) {
-            return true
-        }
-
-        if(rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if(rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if(rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-
-        if(rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid;
-        }
-
-        if(rules.isEmail) {
-            const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            isValid = pattern.test(value) && isValid;
-        }
-
-        // should contain at least one digit, one lower case, one upper case and minimum 6 characters
-        if(rules.isSafePassword) {
-            const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/;
-            isValid = pattern.test(value) && isValid;
-        }
-
-        return isValid;
-    }
-
     inputChangedHandler = (event, controlName) => {
         const updatedControls = updateObject( this.state.controls, {
            [controlName]: updateObject( this.state.controls[controlName],{
                value: event.target.value,
-               valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+               valid: checkValidity(event.target.value, this.state.controls[controlName].validation),
                touched: true
            } )
         } );
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,40 @@ export const updateObject =(oldObject, updatedProperties) => {
         ...updatedProperties
     };
 };
+
+export const checkValidity = (value, rules) => {
+    let isValid = true;
+    if(!rules) {
+        return true
+    }
+
+    if(rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if(rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if(rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if(rules.isNumeric) {
+        const pattern = /^\d+$/;
+        isValid = pattern.test(value) && isValid;
+    }
+
+    if(rules.isEmail) {
+        const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        isValid = pattern.test(value) && isValid;
+    }
+
+    // should contain at least one digit, one lower case, one upper case and minimum 6 characters
+    if(rules.isSafePassword) {
+        const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/;
+        isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+};
